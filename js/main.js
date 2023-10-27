3472d75eb67b3eb3fab54eb24554f8a5112d2890
Jeff Mancilla <jeffmancilla@outlook.com>
2023-10-27 10:15:20 -0500

    set up html, some css, js init

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 console.log('js:loaded')
 
 /*----- constants -----*/
-
+const INIT_STATE = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0
+}
 
 /*----- state variables -----*/
 
+let state = {...INIT_STATE}
+
+let boredom
+let hunger
+let sleepiness
+
+let age
+let cycles
+
+let interval
+let timer
 
 /*----- cached elements  -----*/
 
@@ -13,3 +28,22 @@ console.log('js:loaded')
 
 
 /*----- functions -----*/
+const runGame = () => {
+    console.log('game interval')
+}
+
+const init = () => {
+    state = {...INIT_STATE}
+
+    age = 0
+    cycles = 0
+    interval = 3000 //ms
+
+    timer = setInterval(runGame, interval)
+
+
+    console.log('game initialized')
+}
+
+// initialize
+init()
